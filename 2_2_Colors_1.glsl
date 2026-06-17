86cade6243430f47690e7e4ce23f57510323f4a6
aukvane <aukvane@gmail.com>
2026-06-17 23:19:17 +0800

    chap2

---
+++ 2_2_Colors_1.glsl
@@ -0,0 +1,19 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+vec3 colorA = vec3(1.0, 0.6353, 0.8588);
+vec3 colorB = vec3(1.0, 0.9529, 0.5137);
+
+void main() {
+    vec3 color = vec3(0.0);
+    //初始化颜色color=(0.0,0.0,0.0)
+    
+    float pct= abs(sin(u_time));
+    color=mix(colorA,colorB,pct);
+
+    gl_FragColor = vec4(color,1.0);
+}
---
+++ 2_2_Colors_2.glsl
@@ -0,0 +1,39 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+//basic gradient (1)
+vec3 colorA = vec3(0.000,0.610,0.912);
+vec3 colorB = vec3(1.000,0.971,0.012);
+
+//plot the line(1)
+float plot (vec2 st, float pct){
+  return  smoothstep( pct-0.5, pct, st.y)-
+          smoothstep( pct, pct+0.5, st.y);
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec3 color = vec3(0.0);
+    vec3 pct = vec3(st.x);
+
+    // pct.r = smoothstep(0.0,1.0, st.x);
+    // pct.g = sin(st.x*PI);
+    // pct.b = pow(st.x,0.5);
+
+    //basic gradient (2)
+    color = mix(colorA, colorB, pct);
+
+    // Plot the line in each channel (2)
+    color = mix(color,vec3(1.0,0.0,0.0),plot(st,pct.r));
+    color = mix(color,vec3(0.0,1.0,0.0),plot(st,pct.g));
+    color = mix(color,vec3(0.0,0.0,1.0),plot(st,pct.b));
+
+    gl_FragColor = vec4(color,1.0);
+}
